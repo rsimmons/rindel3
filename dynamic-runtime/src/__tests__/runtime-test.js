6af3ea3ac41f290fb3d45bfe99b30964c6ba3b0a
Russel Simmons <symstym@mac.com>
2017-11-25 15:56:41 -0800

    Add another runtime test, try using mock functions

--- dynamic-runtime/src/__tests__/runtime-test.js
+++ dynamic-runtime/src/__tests__/runtime-test.js
@@ -13,15 +13,39 @@ describe('runtime', () => {
 
     def.addConnection(def.definitionInputs[0], def.definitionOutput);
 
-    const outputs = [];
-    const act = def.activate([{value: 123, changed: true}], (output) => {
-      outputs.push(output);
-    });
+    const outputCallback = jest.fn();
+    const act = def.activate([{value: 123, changed: true}], outputCallback);
 
-    expect(outputs).toEqual([123]);
+    expect(outputCallback.mock.calls).toEqual([[123]]);
+    outputCallback.mockClear();
 
     act.update([{value: 456, changed: true}]);
 
-    expect(outputs).toEqual([123, 456]);
+    expect(outputCallback.mock.calls).toEqual([[456]]);
+  });
+
+  test('user defined identity function, adding connection after activation', () => {
+    const def = new UserDefinition(null, {
+      inputs: [
+        {tempo: 'step'},
+      ],
+      output: {tempo: 'step'},
+    });
+
+    const outputCallback = jest.fn();
+    const act = def.activate([{value: 123, changed: true}], outputCallback);
+
+    // TODO: We might change expectation to be that outputCallback is called with undefined value
+    expect(outputCallback).not.toBeCalled();
+    outputCallback.mockClear();
+
+    def.addConnection(def.definitionInputs[0], def.definitionOutput);
+
+    expect(outputCallback.mock.calls).toEqual([[123]]);
+    outputCallback.mockClear();
+
+    act.update([{value: 456, changed: true}]);
+
+    expect(outputCallback.mock.calls).toEqual([[456]]);
   });
 });
