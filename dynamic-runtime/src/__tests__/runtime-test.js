4dd3d61a2bd2f257043f10e1c27de9ba44474e3d
Russel Simmons <symstym@mac.com>
2017-11-25 15:02:52 -0800

    Add first real runtime test

--- dynamic-runtime/src/__tests__/runtime-test.js
+++ dynamic-runtime/src/__tests__/runtime-test.js
@@ -3,7 +3,25 @@
 import UserDefinition from '../UserDefinition';
 
 describe('runtime', () => {
-  it('should instantiate UserDefinition', () => {
-    expect(new UserDefinition());
+  test('user defined identity function', () => {
+    const def = new UserDefinition(null, {
+      inputs: [
+        {tempo: 'step'},
+      ],
+      output: {tempo: 'step'},
+    });
+
+    def.addConnection(def.definitionInputs[0], def.definitionOutput);
+
+    const outputs = [];
+    const act = def.activate([{value: 123, changed: true}], (output) => {
+      outputs.push(output);
+    });
+
+    expect(outputs).toEqual([123]);
+
+    act.update([{value: 456, changed: true}]);
+
+    expect(outputs).toEqual([123, 456]);
   });
 });
