6ea618b8451f1334ab32e5ea1ffc651c710a2013
Russel Simmons <symstym@mac.com>
2017-11-25 14:13:47 -0800

    Move bulk of runtime code into src subdir

--- dynamic-runtime/index.js
+++ dynamic-runtime/index.js
@@ -1,4 +1,4 @@
-import UserDefinition from './UserDefinition';
+import UserDefinition from './src/UserDefinition';
 
 // Create a new (initially empty) user-defined function definition at the root level (not contained within another definition)
 export function createRootUserDefinition() {
